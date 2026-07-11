d978cdebbfb614d0f9e7b33b8bfaf1fa09abdc07
Cynt4 <jacek.smoliniec@gmail.com>
2026-07-11 18:17:35 +0200

    Added Playwright Initialization and changed folder paths

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
-const DATA_FILE = path.join(__dirname, 'data.json');
+const DATA_FILE = process.env.NODE_ENV === 'test' 
+? path.join(__dirname, 'test-data.json')
+: path.join(__dirname, 'data-json');
+// const DATA_FILE = path.join(__dirname, 'data.json');
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(__dirname)); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 // POBIERANIE Z ZABEZPIECZENIEM
 app.get('/api/data', (req, res) => {
